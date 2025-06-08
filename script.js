efebab838ae24e2ee05f72cafc265bdd0d0828d5
satishcoderZZ1 <167805933+satishcoderZZ1@users.noreply.github.com>
2025-06-08 12:00:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  let heap=[...arr];
+	heap.sort((a,b)=>a-b);
+
+	let totalCost=0;
+	while(heap.length>1){
+		let first=heap.shift();
+		let second=heap.shift();
+
+		let cost=first+second;
+		totalCost+=cost;
+
+		let i=0;
+		while(i<heap.length && heap[i]<cost) i++;
+		heap.splice(i,0,cost);
+	}
+	return totalCost;
 }
 
 module.exports=mincost;
